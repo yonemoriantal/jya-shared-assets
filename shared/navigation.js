853f45758d2ca60da7f70f291a04ebf5124e59f5
yonemoriantal <john.yonemori.antal@outlook.com>
2026-06-30 10:39:50 -0500

    Update navigation.js

--- shared/navigation.js
+++ shared/navigation.js
@@ -1,1 +1,7 @@
-
+document.getElementById("cross-site-nav").innerHTML = `
+<div class="xnav">
+  <a href="https://jyainc.com">Mission-Critical</a>
+  <a href="https://homes-hear-specialist.com">HOMES / HEAR</a>
+  <a href="https://jya-rural-engineering.com">Rural Infrastructure</a>
+</div>
+`;
